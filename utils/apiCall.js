0bf82d69cd37a42eeedb1bfbb1b97b40d5c31703
Tahiru Abdullai <abdullai.tahiru@gmail.com>
2024-07-30 21:54:06 +0000

    chore: Update BASE_API_URL to use NEXT_PUBLIC_API_URL

--- utils/apiCall.js
+++ utils/apiCall.js
@@ -1,5 +1,5 @@
 
-const BASE_API_URL = process.env.REACT_APP_BASE_API_URL || 'http://127.0.0.1:8000/api';
+const BASE_API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8000/api';
 
 const  convertDataToFormData = (data) => {
     let formData = new FormData();
